a794542aa35dc5445aaf61c632f1f2c1a3009c76
Eduard <42778171+eduhh94@users.noreply.github.com>
2023-11-20 21:34:13 +0100

    improvements

--- content.ts
+++ content.ts
@@ -1,5 +1,10 @@
 import { evaluateProfile } from "./utils/api-service.utils"
-import { injectDataIntoDom } from "./utils/dom-manipulation.utils"
+import {
+  createOverlay,
+  injectDataIntoDom,
+  injectWaitingNoticeIntoDom,
+  removeOverlay
+} from "./utils/dom-manipulation.utils"
 import { extractProfile } from "./utils/profile-extraction"
 import { scrollToBottom } from "./utils/scroll-to-bottom.utils"
 import { getStorageData } from "./utils/storage.utils"
@@ -17,8 +22,10 @@ chrome.runtime.onMessage.addListener(
 async function extractAndEvaluateProfiles() {
   console.log("Extracting data from candidate list")
   try {
+    createOverlay() // Create the overlay before scrolling
     await scrollToBottom()
     window.scrollTo(0, 0) // Scroll to top after reaching bottom
+    removeOverlay() // Remove the overlay after scrolling
 
     const profileList = document.querySelectorAll(
       ".ember-view.profile-list > li"
@@ -32,6 +39,9 @@ async function extractAndEvaluateProfiles() {
 }
 
 async function processProfileList(profileList) {
+  // Inject loading notice for each profile
+  profileList.forEach((profile) => injectWaitingNoticeIntoDom(profile))
+
   const jobDescription = await getStorageData("jobDescription")
   for (const profile of profileList) {
     const extractedProfile = await extractProfile(profile)
--- utils/dom-manipulation.utils.ts
+++ utils/dom-manipulation.utils.ts
@@ -1,19 +1,128 @@
-export function injectDataIntoDom(profile: Element, profileEvaluation) {
-  const rating = profileEvaluation.data.rating
-  const explanation = profileEvaluation.data.explanation
-  // Inject response into DOM
+// Injects data into the DOM after profile evaluation
+export function injectDataIntoDom(profile, profileEvaluation) {
+  removeLoadingIndicator(profile)
+
+  const { rating, explanation } = profileEvaluation.data
+
   const targetElement = profile.querySelector(
     ".profile-list__border-bottom .history-group"
-  ) // Adjust the selector as necessary
+  )
 
-  const injectedDataHtml = `
-        <div style="margin-top: 20px; padding: 16px; background-color: #f3f6f8; border-radius: 8px; border: 1px solid #dce0e0; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
-        <div style="font-weight: 800; font-size: 16px; color: #303030; margin-bottom: 8px;">Rating: ${rating}</div>
-          <div style="font-weight: 600; font-size: 14px; color: #303030; margin-bottom: 8px;">Role Fit Rating Explained</div>
-          <div style="font-size: 14px; line-height: 1.5; color: #303030;">${explanation}</div>
-        </div>
-      `
+  if (targetElement) {
+    const injectedDataHtml = buildDataHtml(rating, explanation)
+    targetElement.insertAdjacentHTML("afterend", injectedDataHtml)
+  } else {
+    console.error("Target element for injecting data not found.")
+  }
+}
 
-  // Insert the HTML snippet after the target element
-  targetElement.insertAdjacentHTML("afterend", injectedDataHtml)
+// Removes the loading indicator
+function removeLoadingIndicator(profile) {
+  const loadingIndicator = profile.querySelector(".loading-indicator")
+  if (loadingIndicator) {
+    loadingIndicator.remove()
+  }
+}
+
+// Builds HTML string for injected data
+function buildDataHtml(rating, explanation) {
+  return `
+    <div style="position: relative; margin-top: 20px; padding: 16px; background-color: #f3f6f8; border-radius: 8px; border: 1px solid #dce0e0; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+      <img src="https://i.ibb.co/MVCGgq2/logo.png" alt="Logo" style="position: absolute; top: 10px; right: 10px; width: 30px; height: auto;">
+      <div style="font-weight: 800; font-size: 16px; color: #303030; margin-bottom: 8px;">Rating: ${rating}</div>
+      <div style="font-weight: 600; font-size: 14px; color: #303030; margin-bottom: 8px;">Role Fit Rating Explained</div>
+      <div style="font-size: 14px; line-height: 1.5; color: #303030;">${explanation}</div>
+    </div>
+  `
+}
+
+// Injects a loading notice into the DOM
+export function injectWaitingNoticeIntoDom(profile) {
+  const targetElement = profile.querySelector(
+    ".profile-list__border-bottom .history-group"
+  )
+
+  const loadingHtml = buildLoadingHtml()
+  targetElement.insertAdjacentHTML("afterend", loadingHtml)
+}
+
+// Builds HTML string for loading indicator
+function buildLoadingHtml() {
+  return `
+    <div class="loading-indicator" style="position: relative; margin-top: 20px; padding: 16px; background-color: #f3f6f8; border-radius: 8px; border: 1px solid #dce0e0; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); display: flex; justify-content: center; align-items: center;">
+      <img src="https://i.ibb.co/MVCGgq2/logo.png" alt="Logo" style="position: absolute; top: 10px; right: 10px; width: 30px; height: auto;">
+      <div style="text-align: center;">
+        <div class="circular-loader"></div>
+        <p style="margin-top: 10px;">Loading...</p>
+      </div>
+    </div>
+  `
+}
+
+// Injects CSS styles for overlay and loading animation into the document
+function injectStyles() {
+  const styles = `
+    @keyframes spinner {
+      to { transform: rotate(360deg); }
+    }
+    .circular-loader {
+      border: 4px solid rgba(0, 0, 0, 0.1);
+      border-radius: 50%;
+      border-top: 4px solid #0078D4;
+      width: 24px;
+      height: 24px;
+      animation: spinner 1s linear infinite;
+      margin: 0 auto;
+      margin-bottom: 10px;
+    }
+    #scanningOverlay {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: rgba(0, 0, 0, 0.6);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      z-index: 1000;
+      text-align: center;
+    }
+    #scanningOverlay img {
+      width: 60px;
+      height: auto;
+      margin-bottom: 20px;
+    }
+    #scanningOverlay p {
+      color: white;
+      font-size: 24px;
+      font-weight: bold;
+    }
+  `
+
+  const styleTag = document.createElement("style")
+  styleTag.appendChild(document.createTextNode(styles))
+  document.head.appendChild(styleTag)
+}
+
+injectStyles() // Call this as soon as possible, ideally when the script starts
+
+// Overlay creation and removal
+export function createOverlay() {
+  const overlayHtml = `
+    <div id="scanningOverlay">
+      <div>
+        <img src="https://i.ibb.co/9H8kg2n/logo-dark.png" alt="Logo">
+        <p>Scanning candidates</p>
+      </div>
+    </div>
+  `
+  document.body.insertAdjacentHTML("beforeend", overlayHtml)
+}
+
+export function removeOverlay() {
+  const overlay = document.getElementById("scanningOverlay")
+  if (overlay) {
+    overlay.remove()
+  }
 }
