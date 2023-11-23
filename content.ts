383a06b4771ad3acb05e32fb5833914c9513ab78
Eduard <42778171+eduhh94@users.noreply.github.com>
2023-11-23 19:08:24 +0100

    refactor

--- content.ts
+++ content.ts
@@ -17,6 +17,7 @@ injectControlPanel()
 
 chrome.runtime.onMessage.addListener(
   async function (request, sender, sendResponse) {
+    console.log("content chrome runtime", request)
     if (request.action === "extract-and-analyze-profile-data") {
       extractAndEvaluateProfiles()
     }
@@ -26,20 +27,25 @@ chrome.runtime.onMessage.addListener(
 async function extractAndEvaluateProfiles() {
   console.log("Extracting data from candidate list")
   try {
-    createOverlay() // Create the overlay before scrolling
-    await scrollToBottom()
-    window.scrollTo(0, 0) // Scroll to top after reaching bottom
-    removeOverlay() // Remove the overlay after scrolling
-
-    const profileList = document.querySelectorAll(
-      ".ember-view.profile-list > li"
-    )
-    console.log("Profiles found:", profileList.length)
-
+    await scrollPage()
+    const profileList = getProfiles()
     processProfileList(profileList)
   } catch (err) {
     console.log("extractAndEvaluateProfiles error:", err)
   }
+}
+
+async function scrollPage() {
+  createOverlay() // Create the overlay before scrolling
+  await scrollToBottom()
+  window.scrollTo(0, 0) // Scroll to top after reaching bottom
+  removeOverlay() // Remove the overlay after scrolling
+}
+
+function getProfiles() {
+  const profileList = document.querySelectorAll(".ember-view.profile-list > li")
+  console.log("Profiles found:", profileList.length)
+  return profileList
 }
 
 async function processProfileList(profileList) {
@@ -48,9 +54,15 @@ async function processProfileList(profileList) {
 
   const jobDescription = await getStorageData("jobDescription")
   for (const profile of profileList) {
-    const extractedProfile = await extractProfile(profile)
-    evaluateProfile(extractedProfile, jobDescription, (profileEvaluation) => {
-      injectDataIntoDom(profile, profileEvaluation)
-    })
+    const profileUrl = profile.querySelector("a")?.href?.split("?")?.[0]
+    const extractedProfile = await extractProfile(profileUrl)
+    evaluateProfile(
+      profileUrl,
+      extractedProfile,
+      jobDescription,
+      (profileEvaluation) => {
+        injectDataIntoDom(profile, profileEvaluation)
+      }
+    )
   }
 }
--- ui-components/basic-buttons.component.tsx
+++ ui-components/basic-buttons.component.tsx
@@ -2,11 +2,25 @@ import Button from "@mui/material/Button"
 import Stack from "@mui/material/Stack"
 import * as React from "react"
 
+import { sendMessageToContentScript } from "~utils/message.utils"
+
 export default function BasicButtons() {
   return (
     <Stack spacing={2} direction="row">
       <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
+      <Button
+        variant="contained"
+        onClick={() => {
+          console.log("clicked1", document)
+          chrome.runtime.sendMessage(
+            { action: "someAction" },
+            function (response) {
+              console.log("Response:", response)
+            }
+          )
+        }}>
+        Contained - Start scanning
+      </Button>
       <Button variant="outlined">Outlined</Button>
     </Stack>
   )
