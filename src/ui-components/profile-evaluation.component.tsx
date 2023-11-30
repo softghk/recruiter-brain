df544d0074bbff247cf1963ef66f3778b33a03cd
uluksamakov <webstar0411@gmail.com>
2023-11-30 17:36:23 +0200

    modify builddataHTML

---
+++ src/ui-components/profile-evaluation.component.tsx
@@ -0,0 +1,78 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+import { useStorage } from "@plasmohq/storage/hook"
+
+import { EXTENSION_ENABLE } from "~src/config/storage.config"
+
+const ProfileEvaluation = ({ rating, explanation }) => {
+  const [state, setState] = useStorage<boolean>(EXTENSION_ENABLE)
+
+  const formattedExplanation = explanation.replace(/\n/g, "<br>")
+
+  if (!state) return null
+
+  return (
+    <div
+      style={{
+        position: "relative",
+        marginTop: 20,
+        padding: 16,
+        backgroundColor: "#f3f6f8",
+        borderRadius: 8,
+        border: "1px solid #dce0e0",
+        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+        wordWrap: "break-word"
+      }}>
+      <img
+        src="https://i.ibb.co/MVCGgq2/logo.png"
+        alt="Logo"
+        style={{
+          position: "absolute",
+          top: 10,
+          right: 10,
+          width: 30,
+          height: "auto"
+        }}
+      />
+      <div
+        style={{
+          fontWeight: 800,
+          fontSize: 16,
+          color: "#303030",
+          marginBottom: 8
+        }}>
+        Rating: {rating}
+      </div>
+      <div
+        style={{
+          fontWeight: 600,
+          fontSize: 14,
+          color: "#303030",
+          marginBottom: 8
+        }}>
+        Role Fit Rating Explained
+      </div>
+      <div
+        style={{
+          fontSize: 14,
+          lineHeight: 1.5,
+          color: "#303030"
+        }}>
+        {formattedExplanation}
+      </div>
+    </div>
+  )
+}
+
+const profileContainer = (rating, explanation) => {
+  const container = document.createElement("div")
+  const shadowContainer = container.attachShadow({ mode: "open" })
+  const shadowRootElement = document.createElement("div")
+  shadowContainer.appendChild(shadowRootElement)
+
+  const root = ReactDOM.createRoot(shadowRootElement as HTMLElement)
+  root.render(<ProfileEvaluation rating={rating} explanation={explanation} />)
+}
+
+export default profileContainer
--- src/utils/dom-manipulation.utils.ts
+++ src/utils/dom-manipulation.utils.ts
@@ -1,3 +1,5 @@
+import buildDataHTML from "src/ui-components/profile-evaluation.component"
+
 // Injects data into the DOM after profile evaluation
 export function injectDataIntoDom(profile, profileEvaluation) {
   removeLoadingIndicator(profile)
@@ -7,8 +9,10 @@ export function injectDataIntoDom(profile, profileEvaluation) {
   const targetElement = profile.querySelector(".artdeco-entity-lockup")
 
   if (targetElement) {
-    const injectedDataHtml = buildDataHtml(rating, explanation)
-    targetElement.insertAdjacentHTML("afterend", injectedDataHtml)
+    targetElement.insertAdjacentHTML(
+      "afterend",
+      buildDataHTML(rating, explanation)
+    )
   } else {
     console.error("Target element for injecting data not found.")
   }
@@ -20,20 +24,6 @@ function removeLoadingIndicator(profile) {
   if (loadingIndicator) {
     loadingIndicator.remove()
   }
-}
-
-// Builds HTML string for injected data
-function buildDataHtml(rating, explanation) {
-  const formattedExplanation = explanation.replace(/\n/g, "<br>")
-
-  return `
-    <div class="recruiter-brain-profile-evaluation" style="position: relative; margin-top: 20px; padding: 16px; background-color: #f3f6f8; border-radius: 8px; border: 1px solid #dce0e0; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); word-wrap: break-word;">
-      <img src="https://i.ibb.co/MVCGgq2/logo.png" alt="Logo" style="position: absolute; top: 10px; right: 10px; width: 30px; height: auto;">
-      <div style="font-weight: 800; font-size: 16px; color: #303030; margin-bottom: 8px;">Rating: ${rating}</div>
-      <div style="font-weight: 600; font-size: 14px; color: #303030; margin-bottom: 8px;">Role Fit Rating Explained</div>
-      <div style="font-size: 14px; line-height: 1.5; color: #303030;">${formattedExplanation}</div>
-    </div>
-  `
 }
 
 // Injects a loading notice into the DOM
