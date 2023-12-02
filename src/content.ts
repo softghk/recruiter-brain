b58db3e8d5c7be4aac11604ec55569a4a3b0132d
Eduard <42778171+eduhh94@users.noreply.github.com>
2023-12-02 19:10:55 +0100

    refactoring

--- src/content.ts
+++ src/content.ts
@@ -9,8 +9,21 @@ import { injectDataIntoDom } from "./utils/dom-manipulation.utils"
 import { generateMD5 } from "./utils/hash.utils"
 import { injectControlPanel } from "./utils/inject-control-panel.utils"
 import { requestDataFromIndexedDB } from "./utils/storage.utils"
+import { waitForElement2 } from "./utils/wait-for-element.utils"
 
 export {}
+
+let previousURL = ""
+
+window.addEventListener("DOMNodeInserted", function () {
+  const currentURL = window.location.href
+  if (currentURL !== previousURL) {
+    console.log("URL changed to:", currentURL)
+    previousURL = currentURL
+    injectControlPanel()
+    autoInject()
+  }
+})
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action) {
@@ -19,17 +32,72 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   return true
 })
 
-async function waitForElement2(selector) {
-  return new Promise((resolve) => {
-    const observer = new MutationObserver(() => {
-      if (document.querySelector(selector)) {
-        observer.disconnect()
-        resolve("")
+async function autoInject() {
+  const listElementSelector = "ol.profile-list"
+  const parentSelector = ".page-layout__workspace"
+
+  let listObserver = null // Variable to store the current observer for the list
+
+  // Function to observe the OL element
+  const observeListElement = () => {
+    if (listObserver) {
+      listObserver.disconnect() // Disconnect any existing observer
+      console.log("Previous list observer disconnected.")
+    }
+
+    const olElement = document.querySelector(listElementSelector)
+    if (!olElement) {
+      console.warn("OL element not found:", listElementSelector)
+      return
+    }
+
+    listObserver = new MutationObserver((mutations) =>
+      mutations.forEach(handleMutation)
+    )
+
+    listObserver.observe(olElement, {
+      attributes: true,
+      attributeOldValue: true,
+      attributeFilter: ["class"],
+      subtree: true
+    })
+    console.log("OL element observer connected.")
+  }
+
+  // Observe the parent element for the addition or removal of the OL element
+  const parentObserver = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      if (mutation.type === "childList") {
+        const hasOlElement = Array.from(mutation.addedNodes)
+          .concat(Array.from(mutation.removedNodes))
+          .some(
+            (node) =>
+              node instanceof Element &&
+              node.matches &&
+              node.matches(listElementSelector)
+          )
+
+        if (hasOlElement) {
+          observeListElement()
+        }
       }
     })
-
-    observer.observe(document.body, { childList: true, subtree: true })
   })
+
+  // Wait for the parent element before setting up the observer
+  await waitForElement2(parentSelector)
+  const parentElement = document.querySelector(parentSelector)
+  if (!parentElement) {
+    console.error("Parent element not found:", parentSelector)
+    return
+  }
+
+  parentObserver.observe(parentElement, { childList: true, subtree: true })
+
+  // Initial observation of the OL element
+  await waitForElement2(listElementSelector)
+  console.log("Initial OL is available")
+  observeListElement()
 }
 
 async function handleMutation(mutation) {
@@ -120,85 +188,3 @@ async function getEvaluationData(projectId, jobDescriptionId, profileId) {
     }
   })
 }
-
-async function autoInject() {
-  const listElementSelector = "ol.profile-list"
-  const parentSelector = ".page-layout__workspace"
-
-  let listObserver = null // Variable to store the current observer for the list
-
-  // Function to observe the OL element
-  const observeListElement = () => {
-    if (listObserver) {
-      listObserver.disconnect() // Disconnect any existing observer
-      console.log("Previous list observer disconnected.")
-    }
-
-    const olElement = document.querySelector(listElementSelector)
-    if (!olElement) {
-      console.warn("OL element not found:", listElementSelector)
-      return
-    }
-
-    listObserver = new MutationObserver((mutations) =>
-      mutations.forEach(handleMutation)
-    )
-
-    listObserver.observe(olElement, {
-      attributes: true,
-      attributeOldValue: true,
-      attributeFilter: ["class"],
-      subtree: true
-    })
-    console.log("OL element observer connected.")
-  }
-
-  // Observe the parent element for the addition or removal of the OL element
-  const parentObserver = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-      if (mutation.type === "childList") {
-        const hasOlElement = Array.from(mutation.addedNodes)
-          .concat(Array.from(mutation.removedNodes))
-          .some(
-            (node) =>
-              node instanceof Element &&
-              node.matches &&
-              node.matches(listElementSelector)
-          )
-
-        if (hasOlElement) {
-          observeListElement()
-        }
-      }
-    })
-  })
-
-  // Wait for the parent element before setting up the observer
-  await waitForElement2(parentSelector)
-  const parentElement = document.querySelector(parentSelector)
-  if (!parentElement) {
-    console.error("Parent element not found:", parentSelector)
-    return
-  }
-
-  parentObserver.observe(parentElement, { childList: true, subtree: true })
-
-  // Initial observation of the OL element
-  await waitForElement2(listElementSelector)
-  console.log("Initial OL is available")
-  observeListElement()
-}
-
-//injectControlPanel()
-
-let previousURL = ""
-
-window.addEventListener("DOMNodeInserted", function () {
-  const currentURL = window.location.href
-  if (currentURL !== previousURL) {
-    console.log("URL changed to:", currentURL)
-    previousURL = currentURL
-    injectControlPanel()
-    autoInject()
-  }
-})
