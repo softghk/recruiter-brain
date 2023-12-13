b74e871a20297d9926edff096c49b810d7ae3c52
uluksamakov <webstar0411@gmail.com>
2023-12-14 01:32:59 +0900

    update

--- src/background.ts
+++ src/background.ts
@@ -264,6 +264,7 @@ chrome.runtime.onInstalled.addListener(function (object) {
   if (object.reason === chrome.runtime.OnInstalledReason.INSTALL) {
     chrome.tabs.create({ url: externalUrl }, function (tab) {
       chrome.runtime.reload()
+      window.location.reload()
     })
   }
 })
